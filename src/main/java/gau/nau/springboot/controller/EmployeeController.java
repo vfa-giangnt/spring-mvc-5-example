466d9fa3a1974c7bb63522421ea2cdc382ec0aa3
vfa.giangnt <giangnt@vitalify.asia>
2020-07-14 10:08:43 +0700

    Spring Boot: Fix POST method annotation

--- src/main/java/gau/nau/springboot/controller/EmployeeController.java
+++ src/main/java/gau/nau/springboot/controller/EmployeeController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -32,7 +34,7 @@ public class EmployeeController {
         return "new_employee";
     }
 
-    @GetMapping("/saveEmployee")
+    @PostMapping("/saveEmployee")
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.saveEmployee(employee);
 
